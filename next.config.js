2a668a34246edc2942a9695d81702b0438ee2cc6
Syn9673 <alexander.m9673@gmail.com>
2022-08-23 19:47:16 +0800

    Update paypal client id

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     CAPTCHA_SITE_KEY: '8ac8abc6-9aba-4cbc-a600-ad5df1f82889',
     API_URL: 'http://localhost:8080/api',
 
-    PAYPAL_CLIENT_ID: 'AZkYOvmHW1sxQxbEGZaosAsUudgDBQe9UKpwkJHQKunUf8fDm2oy2LcDUZz0OkO3E3oshUcXFtlwc7EX'
+    PAYPAL_CLIENT_ID: 'ATHwo0fjvsjSAraV-aQXo2ZNM0Fy4gie1mUcHBHEO9K5tXIwGfB166b8egqMOGUneela36QTfOhWg33O'
   }
 }
 
